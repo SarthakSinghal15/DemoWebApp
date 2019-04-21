a21d4ab338e1caf50d96fc0243bbedee518d915e
Sarthak Singhal <sarthak.singhal@sjsu.edu>
2019-04-21 15:46:00 -0700

    Landing page post route to submit email

--- controllers/landing.js
+++ controllers/landing.js
@@ -3,7 +3,7 @@ exports.get_landing=function(req, res, next) {
   res.render('landing', { title: 'Express' });
 }
 
-exports.submit_lead=function(req, res, next) {
+exports.submit_lead = function(req, res, next) {
 	console.log("lead email is :",req.body.lead_email);
 	res.redirect('/');
 }
